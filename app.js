27c9cec2464e6463594bb0b04b66e5ed2e667ef2
Chuka <b.chuka@gmail.com>
2020-01-20 12:19:15 +0800

    Finance module ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var uiController = (function() {
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, //exp or inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
@@ -23,6 +23,7 @@ var uiController = (function() {
 
 //Санхүүтэй ажиллах контроллер
 var financeController = (function() {
+  //private function
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
@@ -33,8 +34,9 @@ var financeController = (function() {
     this.description = description;
     this.value = value;
   };
+  //private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: []
     },
@@ -43,14 +45,37 @@ var financeController = (function() {
       exp: 0
     }
   };
+  return {
+    addItem: function(type, desc, val) {
+      var item,
+        id = 1;
+      //id
+      if (data.items[type].length === 0) {
+        id = 1;
+      } else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function() {
+      return data;
+    }
+  };
 })();
 
 //Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
   var ctrlAddItem = function() {
     //1. Оруулах өгөгдлийг дэлгэцээс олж авна
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
     //2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
+    console.log(input);
     //3. Олж авсан өгөгдлүүдээ Вэб дээр тохирох хэсэгт харуулна.
     //4. Төсөвийг тооцоолно.
     //5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
