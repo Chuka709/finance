2a4a91e49d77b16c530e53b0dd8384eadaa9a88b
Chuka <b.chuka@gmail.com>
2020-01-20 09:16:37 +0800

    Finance module hiij ehelsen. Expense, Income objectuudiig uusgese

--- app.js
+++ app.js
@@ -1,29 +1,78 @@
 //Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addButton: ".add__btn"
+  };
+  //Доорх public service-үүдээс бусад контроллерууд дуудаж ажиллана.
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
 
 //Санхүүтэй ажиллах контроллер
-var financeController = (function() {})();
+var financeController = (function() {
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  };
+})();
 
 //Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
   var ctrlAddItem = function() {
     //1. Оруулах өгөгдлийг дэлгэцээс олж авна
-    console.log("Дэлгэцээс өгөгдөл авах хэсэг");
-  };
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
+    console.log(uiController.getInput());
     //2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-
     //3. Олж авсан өгөгдлүүдээ Вэб дээр тохирох хэсэгт харуулна.
-
     //4. Төсөвийг тооцоолно.
-
     //5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-  });
-  document.addEventListener("keypress", function(event) {
-    //event.which нь дээр үеийн броүсер үед ажиллана.
-    if (event.keyCode === 13 || event.which === 13) {
+  };
+  var setupEventListeners = function() {
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addButton).addEventListener("click", function() {
       ctrlAddItem();
-    } else console.log(event.keyCode + " кодтой товчыг дарсан");
-  });
+    });
+    document.addEventListener("keypress", function(event) {
+      //event.which нь дээр үеийн броүсер үед ажиллана.
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  return {
+    init: function() {
+      console.log("App started...");
+      setupEventListeners();
+    }
+  };
 })(uiController, financeController);
+
+appController.init();
